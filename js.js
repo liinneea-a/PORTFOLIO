bb1f34ab067db644a8752071ed12abcb38cedd3f
linnea <linnea.albertsson@gmail.com>
2021-12-01 10:57:08 +0100

    start finish about

--- js.js
+++ js.js
@@ -61,6 +61,7 @@ function toggleDropdown() {
     closeMenu.onclick = closeDropDown;
 }
 function closeDropDown() {
+    console.log('hi');
     const toggleMenu = document.getElementById('toggleMenu');
     toggleMenu.style.display = 'none';
 }
