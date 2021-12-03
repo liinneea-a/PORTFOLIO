b208e31725cc30de6f05e54a5bf0e06200573b6d
linnea <linnea.albertsson@gmail.com>
2021-12-03 18:30:57 +0100

    Finished first version

---
+++ README.md
@@ -0,0 +1,8 @@
+# PORTFOLIO
+Linnea Albertsson
+
+Detta är min första version av mitt portfolio. Det innehåller kort information om vem jag är, vad jag har gjort samt ett formulär för att kontakta mig. I nuläget är inte formuläret aktivt. 
+
+Jag har valt att göra begränsningen att låta småfel i designen vara i nuläget för att ta mig tiden att i lugn och ro snygga till det som jag vill ha det. 
+
+ [Demo](https://liinneea-a.github.io/PORTFOLIO/)
--- js.js
+++ js.js
@@ -1,73 +1,29 @@
-window.onload = main;
+/** Starts function main when page is loaded */
+window.addEventListener('load', main);
 
 
-/*----ToggleLightMode-----*/
+/** creates variable for local storage and light toggle button */
 let lightMode = localStorage.getItem('lightMode');
 const lightModeToggle = document.querySelector('#toggleLightMode');
 
-
-//check if LM is enabled
-// if yes = turn it off
-// if disabled = turn it on
-
-const enableLightMode = () => {
-    // add class lightmode to wrapper
-    document.querySelector('#wrapper').classList.add('lightmode');
-    document.querySelector('#about').classList.add('lightmode');
-    document.querySelector('#portfolio').classList.add('lightmode');
-    document.querySelector('#contact').classList.add('lightmode');
-    document.getElementById('wave-bg-s').classList.add('waves-lightmode');
-    document.getElementById('wave-s').classList.add('waves-lightmode');
-
-
-    // update lightmode in localstorage
-    localStorage.setItem('lightMode', 'enabled');
-
-    
-
-};
-
-const disableLightMode = () => {
-    // remove class lightmode to wrapper
-    document.getElementById('wrapper').classList.remove('lightmode');
-    document.querySelector('#about').classList.remove('lightmode');
-    document.querySelector('#portfolio').classList.remove('lightmode');
-    document.querySelector('#contact').classList.remove('lightmode');
-    document.getElementById('wave-bg-s').classList.remove('waves-lightmode');
-    document.getElementById('wave-s').classList.remove('waves-lightmode');
-    // update lightmode in localstorage
-    localStorage.setItem('lightMode', null);
-
-
-};
-
-if (lightMode === 'enabled') {
-    enableLightMode();
-}
-
-lightModeToggle.addEventListener('click', () => {
-    lightMode = localStorage.getItem('lightMode');
-    if (lightMode !== 'enabled') {
-        enableLightMode();
-        // console.log(lightMode);
-    } else {
-        disableLightMode();
-    }
-});
-
-/*---------------------*/
-
+/**  */
 function main() {
     addEventListeners();
 
+    /** Checks if lightmode is enabled when page loads */
+    if (lightMode === 'enabled') {
+    enableLightMode();
+    }
+    
 }
+
+/**  adds onclick for dropdown menu */
 function addEventListeners() {
     const menuBtn = document.getElementById('menuIcon');
-    const closeMenu = document.getElementById('closeMenu');
     menuBtn.addEventListener('click', toggleDropdown);
 } 
 
-
+/** if menu button is clicked (on small screen) menu drop down is shown */
 function toggleDropdown() {
     const toggleMenu = document.getElementById('toggleMenu');
     const openMenu = document.getElementById('openMenu');
@@ -78,16 +34,58 @@ function toggleDropdown() {
     closeMenu.onclick = closeDropDown;
 
 }
+/** closes menu drop down */
 function closeDropDown() {
+    const closeMenu = document.getElementById('closeMenu');
     const toggleMenu = document.getElementById('toggleMenu');
-    // toggleMenu.classList.add('hidedropdownmenu');
-    toggleMenu.addEventListener('click', hide());
+    toggleMenu.style.display = 'none';
+}
 
-    // toggleMenu.style.display = 'none';
-}
-function screenLarge() {
-    console.log('Large');
-}
+
+
+//check if LM is enabled
+// if yes = turn it off
+// if disabled = turn it on
+
+
+/** changes lightmode by checking in local storage if lightmode is enabled 
+ * to trigger right function
+*/
+lightModeToggle.addEventListener('click', () => {
+    lightMode = localStorage.getItem('lightMode');
+    if (lightMode !== 'enabled') {
+        enableLightMode();
+    } else {
+        disableLightMode();
+    }
+});
+
+/** Changes page to lightmode by adding class .lightmode on elements */
+const enableLightMode = () => {
+    // add class lightmode to wrapper
+    document.querySelector('#wrapper').classList.add('lightmode');
+    document.querySelector('#about').classList.add('lightmode');
+    document.querySelector('#portfolio').classList.add('lightmode');
+    document.querySelector('#contact').classList.add('lightmode');
+    document.getElementById('wave-bg-s').classList.add('waves-lightmode');
+    document.getElementById('wave-s').classList.add('waves-lightmode');
+
+    /**Updating lightmode to enabled in LS */
+    localStorage.setItem('lightMode', 'enabled');
+};
+
+/** Enables lightmode by removing class .lightmode on elements */
+const disableLightMode = () => {
+    document.getElementById('wrapper').classList.remove('lightmode');
+    document.querySelector('#about').classList.remove('lightmode');
+    document.querySelector('#portfolio').classList.remove('lightmode');
+    document.querySelector('#contact').classList.remove('lightmode');
+    document.getElementById('wave-bg-s').classList.remove('waves-lightmode');
+    document.getElementById('wave-s').classList.remove('waves-lightmode');
+
+    /** Updates lightmode to null in LS */
+    localStorage.setItem('lightMode', null);
+};
 
 
 // function changeBackgroundColorOnScroll() {
